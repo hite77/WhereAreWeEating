25d6ab2a4edef9d04a5543020658bbf81b43b874
Jason Hite <hite77@gmail.com>
2015-06-08 22:21:33 -0400

    internet pull for recycler

--- MyApplication/app/src/main/java/whereareweeating/com/hiteware/whereareweeating/MainActivity.java
+++ MyApplication/app/src/main/java/whereareweeating/com/hiteware/whereareweeating/MainActivity.java
@@ -1,38 +1,124 @@
 package whereareweeating.com.hiteware.whereareweeating;
 
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Window;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends Activity {
+    private static final String TAG = "RecyclerViewExample";
+    private List<FeedItem> feedItemList = new ArrayList<FeedItem>();
 
-public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private MyRecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /* Allow activity to show indeterminate progress-bar */
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+
         setContentView(R.layout.activity_main);
+
+        /* Initialize recycler view */
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        /*Downloading data from below url*/
+        final String url = "http://javatechig.com/api/get_category_posts/?dev=1&slug=android";
+        new AsyncHttpTask().execute(url);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public class AsyncHttpTask extends AsyncTask<String, Void, Integer> {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        @Override
+        protected void onPreExecute() {
+            setProgressBarIndeterminateVisibility(true);
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        protected Integer doInBackground(String... params) {
+            InputStream inputStream = null;
+            Integer result = 0;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                /* forming th java.net.URL object */
+                URL url = new URL(params[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                /* for Get request */
+                urlConnection.setRequestMethod("GET");
+                int statusCode = urlConnection.getResponseCode();
+
+                /* 200 represents HTTP OK */
+                if (statusCode ==  200) {
+                    BufferedReader r = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = r.readLine()) != null) {
+                        response.append(line);
+                    }
+
+                    parseResult(response.toString());
+                    result = 1; // Successful
+                }else{
+                    result = 0; //"Failed to fetch data!";
+                }
+
+            } catch (Exception e) {
+                Log.d(TAG, e.getLocalizedMessage());
+            }
+            return result; //"Failed to fetch data!";
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            setProgressBarIndeterminateVisibility(false);
+            /* Download complete. Lets update UI */
+            if (result == 1) {
+                adapter = new MyRecyclerAdapter(MainActivity.this, feedItemList);
+                mRecyclerView.setAdapter(adapter);
+            } else {
+                Log.e(TAG, "Failed to fetch data!");
+            }
+        }
+    }
+
+    private void parseResult(String result) {
+        try {
+            JSONObject response = new JSONObject(result);
+            JSONArray posts = response.optJSONArray("posts");
+
+            /*Initialize array if null*/
+            if (null == feedItemList) {
+                feedItemList = new ArrayList<FeedItem>();
+            }
+
+            for (int i = 0; i < posts.length(); i++) {
+                JSONObject post = posts.optJSONObject(i);
+
+                FeedItem item = new FeedItem();
+                item.setTitle(post.optString("title"));
+                item.setThumbnail(post.optString("thumbnail"));
+                feedItemList.add(item);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
